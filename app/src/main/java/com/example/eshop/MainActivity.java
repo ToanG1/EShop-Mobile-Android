c1125bd4c6689cd4d25e3dfaa8a90f9a9c811a3d
ToanDinh <Toandinh01675@gmail.com>
2023-03-17 00:42:56 +0700

    components...

--- app/src/main/java/com/example/eshop/MainActivity.java
+++ app/src/main/java/com/example/eshop/MainActivity.java
@@ -1,14 +1,25 @@
 package com.example.eshop;
 
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.components);
+        setContentView(R.layout.activity_main);
+//        RangeSlider rangeSlider = findViewById(R.id.my_range_slider);
+//        rangeSlider.setValues(1f, 10f);
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        NavController navController = Navigation.findNavController(this,  R.id.fragment);
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
     }
 }
